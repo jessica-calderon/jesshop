f57b59378f4a4ae93f40af05d6217b6b915ea717
jessica-calderon <calderonjessica13@yahoo.com>
2022-07-29 18:00:08 -0500

    update readme

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -37,7 +37,7 @@ router.get("/:id", (req, res) => {
   })
     .then((dbCategoryData) => {
       if (!dbCategoryData) {
-        res.status(404).json({ message: "No catgeory found with this id" });
+        res.status(404).json({ message: "No category found with this id" });
         return;
       }
       res.json(dbCategoryData);
